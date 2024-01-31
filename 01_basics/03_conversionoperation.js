100a2b7ce853738e2f07f30025012b16d3b2fcc0
AbhishekYadav1999 <158019310+AbhishekYadav1999@users.noreply.github.com>
2024-01-31 08:58:37 +0000

    conversionoperation done

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -21,7 +21,45 @@ console.log(booleanLoggedIn);
 //1=>true ; 0=>false
 //""=>false; "hitesh"=>true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+//************operation***********
+let value=3
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="Hello"
+let str2=" Hitesh"
+let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(true)
+// console.log(1+true);
+// console.log(true+1);
+// console.log(+"");
+
+let num1,num2,num3;
+num1 = num2 = num3 = 2 + 2
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter);
